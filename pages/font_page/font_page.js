cdad16aeba12118dd8a6d3a56c3c45a53d756e75
021800527 <978621025@qq.com>
2020-11-21 10:45:14 +0800

    setdata

--- pages/font_page/font_page.js
+++ pages/font_page/font_page.js
@@ -6,8 +6,25 @@ Page({
    */
   data: {
     jinrishici:'',
+    flag:true
   },
-
+  // 获取用户信息成功与否来擦除按钮
+  test:function(){
+    var that = this
+    wx.getUserInfo({
+      success:function(res){
+        that.setData({
+          flag : false
+        }),
+        wx.navigateTo({
+          url: '../index/index',
+        })
+      },
+      fail:function(){
+        console.log('refuse')
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -19,26 +36,36 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    let that = this
     wx.getUserInfo({
       success:function(res){
-        console.log(res)
+        that.setData({
+          flag : false
+        })
+        setTimeout(function(){
+          wx.navigateTo({
+            url: '../index/index',
+          })
+        },2000)
+      },
+      fail:function(res){
+        that.setData({
+          flag : true
+        })
       }
     })
     wx.getSetting({
       withSubscriptions: true,
       success:function(res){
-        console.log(res)
         if (res.authSetting['scope.userInfo'] === false) {
           wx.login({
             success:res=>{
-              console.log(res.code)
               wx.request({
                // 自行补上自己的 APPID 和 SECRET
                method:'POST',
                url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wx7baf43318101b003&secret=b9858b4fc34320141767e0f4a1d7d0b7&js_code=' + res.code + '&grant_type=authorization_code',
                 success: res => {
                 // 获取到用户的 openid
-                console.log(res);
                 }
                 })
                 }
@@ -46,11 +73,6 @@ Page({
         }
       }
     })
-    // setTimeout(function(){
-    //   wx.navigateTo({
-    //     url: '../index/index',
-    //   })
-    // },3000)
     jinrishici.load(result => {
       // 下面是处理逻辑示例
       this.setData({"jinrishici": result.data.content})
